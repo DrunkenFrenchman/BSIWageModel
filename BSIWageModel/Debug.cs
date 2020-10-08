4e56aa36adf25ab7ab164565c57926f54faa853a
DrunkenFrenchman <72567231+DrunkenFrenchman@users.noreply.github.com>
2020-10-08 11:31:56 -0500

    WIIP

--- BSIWageModel/Debug.cs
+++ BSIWageModel/Debug.cs
@@ -10,7 +10,6 @@ namespace BSIWageModel
     public class Debugger
     {
         private static readonly MySettings settings = MySettings.Instance;
-        private static string pathString;
         private static string fileName = "bsi_wagemodel.debug.log";
 
         //Print Message in Game Helper
