e4e5e3d78c828e3426a7d0f74460f5cd5d2d5b3a
DrunkenFrenchman <72567231+DrunkenFrenchman@users.noreply.github.com>
2020-10-08 23:12:54 -0500

    WIP

--- BSIWageModel/MySettings.cs
+++ BSIWageModel/MySettings.cs
@@ -24,10 +24,10 @@ namespace BSIWageModel
         public int BSIMinWage { get; set; } = 1;
         [SettingPropertyInteger("{=BSIWM_SETTING_01}Maximum Troop Wage", 1, 300, HintText = "{=BSIWM_SETTING_DESC_01}This number determines the maximum wage paid to a troop before mounted and mercenary multipliers are applied. MUST BE LARGER THAN MINIMUM", Order = 2, RequireRestart = false)]
         [SettingPropertyGroup("{=BSIWM_SETTING_GROUP_01}Wage Model", GroupOrder = 0)]
-        public int BSIMaxWage { get; set; } = 64;
+        public int BSIMaxWage { get; set; } = 60;
         [SettingPropertyFloatingInteger("{=BSIWM_SETTING_02}Mounted Troop Multiplier", 1f, 10f, HintText = "{=BSIWM_SETTING_DESC_02}This is a multiplier value used to modify the wage of mounted troops", Order = 3, RequireRestart = false)]
         [SettingPropertyGroup("{=BSIWM_SETTING_GROUP_01}Wage Model", GroupOrder = 0)]
-        public float BSIMountedWageMult { get; set; } = 2f;
+        public float BSIMountedWageMult { get; set; } = 3f;
         [SettingPropertyFloatingInteger("{=BSIWM_SETTING_31}Mercenary Multiplier", 0.1f, 10f, HintText = "{=BSIWM_SETTING_DESC_31}This is a multiplier value used to modify the wage of mercenary troops", Order = 4, RequireRestart = false)]
         [SettingPropertyGroup("{=BSIWM_SETTING_GROUP_01}Wage Model", GroupOrder = 0)]
         public float BSIMercenaryWageMult { get; set; } = 1.2f;
@@ -36,12 +36,12 @@ namespace BSIWageModel
         public float BSIRecruitmentMult { get; set; } = 10f;
         [SettingPropertyFloatingInteger("{=BSIWM_SETTING_37}Troop Strength Curve", 0.1f, 3f, HintText = "{=BSIWM_SETTING_DESC_37}Basically, changes shape of curve. Value below one will make lower incomes higher and higher incomes lower. Value above 1 will make higher wages higher and lower wages lower", Order = 6, RequireRestart = false)]
         [SettingPropertyGroup("{=BSIWM_SETTING_GROUP_01}Wage Model", GroupOrder = 0)]
-        public float BSIStrengthCurve { get; set; } = 0.7f;
+        public float BSIStrengthCurve { get; set; } = 1.3f;
 
         //Main Settings for Food Model
         [SettingPropertyBool("{=BSIWM_SETTING_08}Food Model", HintText = "{=BSIWM_SETTING_DESC_08}NOT YET IMPLEMENTED! Check this to enable 'Food Model'", Order = 0, RequireRestart = false)]
-        [SettingPropertyGroup("{=BSIWM_SETTING_GROUP_03}Food Model", GroupOrder = 1, IsMainToggle = true)]
-        public bool BSIFoodModelToggle { get; set; } = true;
+        [SettingPropertyGroup("{=BSIWM_SETTING_GROUP_03}Food Model [NOT YET IMPLEMENTED]", GroupOrder = 1, IsMainToggle = true)]
+        public bool BSIFoodModelToggle { get; set; } = false;
         [SettingPropertyFloatingInteger("{=BSIWM_SETTING_03}Mounted Troop Food Multiplier", 1f, 5f, HintText = "{=BSIWM_SETTING_DESC_03}Food cumsumption multiplier applied to Mounted Units", Order = 1, RequireRestart = false)]
         [SettingPropertyGroup("{=BSIWM_SETTING_GROUP_02}Food Model", GroupOrder = 1)]
         public float BSIMountedFoodMult { get; set; } = 2f;
@@ -52,7 +52,7 @@ namespace BSIWageModel
         //Mod Compatibilty Options
         [SettingPropertyBool("{=BSIWM_SETTING_BSI}Blood Shit and Iron", HintText = "{=BSIWM_SETTING_DESC_BSI}Check this if you are playing with the Blood Shit and Iron main mod - You really should be...", Order = 0, RequireRestart = true)]
         [SettingPropertyGroup("{=BSIWM_SETTING_GROUP_03}Mod Compatibility Options", GroupOrder = 2)]
-        public bool BSIMainModCompat { get; set; } = false;
+        public bool BSIMainModCompat { get; set; } = true;
 
         //Debugger Toggle
         [SettingPropertyBool("{=BSIWM_SETTING_DEBUG}Wage Model Debug", HintText = "{=BSIWM_SETTING_DESC_DEBUG}Check this to enable Debug mode", Order = 0, RequireRestart = false)]
